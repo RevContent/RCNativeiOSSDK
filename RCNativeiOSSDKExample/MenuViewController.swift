3d581859a47020a7861a948db686e61b2e2b84d9
rakesh <ninjacoderrakesh@gmail.com>
2020-09-15 16:14:29 +0530

    updated example project

---
+++ RCNativeiOSSDKExample/MenuViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  RCNativeiOSSDKExample
+//
+//  Created by Apple on 21/05/20.
+//  Copyright © 2020 Revcontent. All rights reserved.
+//
+
+import UIKit
+import RCNativeiOSSDK
+
+class MenuViewController: UIViewController
+{
+    // Instance
+    var tableData = ["1x6", "1x7",  "1x8", "1/9", "1/10"]
+    let widgetID = ["144708","144710","144711","144712","144713"]
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+// MARK: UITableview Datasource Methods
+extension MenuViewController : UITableViewDataSource
+{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.text = tableData[indexPath.row]
+        cell.textLabel?.textAlignment = .left
+        return cell
+    }
+}
+
+// MARK: UITableview Delegate Method
+extension MenuViewController : UITableViewDelegate
+{
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+    {
+        let widgetId = widgetID[indexPath.row]
+            let vc = storyboard?.instantiateViewController(identifier: "WidgetViewController") as? WidgetViewController
+            vc!.widgetId = widgetId
+            self.navigationController?.pushViewController(vc!, animated: true)
+    }
+}
+
+
+
---
+++ RCNativeiOSSDKExample/WidgetViewController.swift
@@ -0,0 +1,39 @@
+//
+//  WidgetViewController.swift
+//  RCNativeiOSSDKExample
+//
+//  Created by Mayank Palotra on 15/09/20.
+//  Copyright © 2020 Revcontent. All rights reserved.
+//
+
+import UIKit
+import RCNativeiOSSDK
+
+class WidgetViewController: UIViewController {
+
+    @IBOutlet weak var viewWidget : UIView!
+    var widgetId : String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        RCNativeiOSSDK.setup()
+        self.viewWidget.addSubview(self.createWidget(widgetId))
+        // Do any additional setup after loading the view.
+    }
+    
+    func createWidget(_ widId : String) -> RCNactiveJSWidgetView
+    {
+        let widget = RCNactiveJSWidgetView.init(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height))
+        // WidgetId is required.
+        widget.setWidgetId(widgetId: widId)
+        // WidgetSubId is optional.
+        widget.setWidgetSubId(widgetSubId:["category":"entertainment", "utm_code":"123456"]);
+        // baseUrl is optional.
+        widget.setBaseUrl(baseUrl: "https://performance.revcontent.dev")
+        //self.view.addSubview(widget)
+        widget.loadWidget()
+        return widget
+    }
+
+    
+}
