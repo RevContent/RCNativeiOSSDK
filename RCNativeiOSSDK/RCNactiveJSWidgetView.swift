ac49219a756d3dda6afd058727eab2ef582b34c5
Maryan Luchko <ml@leobit.com>
2020-10-19 18:49:51 +0300

    Removed unused code. Fixed layout of scroll view. Fixed layout of example labels. RCNativeiOSSDK refactoring. RCNactiveJSWidgetView refactoring(made init simple). Added RCNativeJSWidgetHeightHandler. Added new test widget ids. Refactored Widget/AnotherWidget view controllers.

--- RCNativeiOSSDK/RCNactiveJSWidgetView.swift
+++ RCNativeiOSSDK/RCNactiveJSWidgetView.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 
-private let defaultBaseURL = "https://performance.revcontent.dev"
+private let defaultBaseURL = "https://performance.revcontent.dev/"
 private let widgetHostKey = "{widget-host}"
 private let widgetHostVal = "habitat"
 private let endPointKey = "{endpoint}"
@@ -26,167 +26,187 @@ private let sourceUrlKey = "{source-url}"
 
 
 public class RCNactiveJSWidgetView: WKWebView {
-    private var htmlWidget:String?
-    private var widgetId:String?
-    private var siteUrl:String?
-    private var baseUrl:String?
+  private var htmlWidget:String?
+  private var widgetId:String?
+  private var siteUrl:String?
+  private var baseUrl:String?
+  private var widgetSubId:[String:String]?
+  
+  private weak var containerView: UIView!
+  
+  lazy var heightContraint: NSLayoutConstraint = {
+    let constraint = NSLayoutConstraint(item: self,
+                                        attribute: .height,
+                                        relatedBy: .equal,
+                                        toItem: nil,
+                                        attribute: .notAnAttribute,
+                                        multiplier: 1,
+                                        constant: containerView.bounds.height)
+    constraint.isActive = true
+    return constraint
+  }()
+  
+  static fileprivate var defaultConfiguration: WKWebViewConfiguration = {
+    let configuration = WKWebViewConfiguration()
+    if #available(iOS 14.0, *) {
+      configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+    } else {
+      configuration.preferences.javaScriptEnabled = true
+    }
+    configuration.allowsInlineMediaPlayback = true
+    return configuration
+  }()
+  
+  lazy var heightHandler: RCNativeJSWidgetHeightHandler = {
+    let handler = RCNativeJSWidgetHeightHandler(widgetView: self)
+    handler.heightDidChange = { [weak self] newHeight in
+      self?.heightContraint.constant = newHeight
+      self?.layoutIfNeeded()
+    }
+    return handler
+  }()
+  
+  public init(containerView: UIView,
+              configuration: WKWebViewConfiguration? = nil) {
+    super.init(frame: containerView.bounds, configuration: configuration ?? RCNactiveJSWidgetView.defaultConfiguration)
+    backgroundColor = .clear
+    navigationDelegate = self
+    self.containerView = containerView
+    commonInit()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  deinit {
+    heightHandler.stopObservingHeight()
+    debugPrint("\(String(describing: self)): deinited")
+  }
+  
+  private func loadHTMLContent(){
+      self.htmlWidget =
+      """
+      <!doctype html>
+      <html>
+      <head>
+      <style>
+      html, body { margin:0; padding: 0; }
+      
+      @media (prefers-color-scheme: dark) {
+      html, body {
+      background: #000;
+      }
+      }
+      </style>
+      </head>
+      <body>
+      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no, shrink-to-fit=no\">
+      <div id=\"rc-widget-27bbf8\" data-rc-widget data-widget-host=\"{widget-host}\" data-endpoint=\"{endpoint}\" data-is-secured=\"{is-secured}\" data-widget-id=\"{widget-id}\" data-sub-ids=\"{sub-ids}\">
+      </div>
+      <script src=\"{js-src}\" defer=\"{defer}\"></script>
+      </body>
+      </html>
+      """
+  }
+  
+  private func commonInit() {
+    translatesAutoresizingMaskIntoConstraints = false;
+    frame = containerView.bounds;
+    containerView.addSubview(self);
+    NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: containerView, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+    NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: containerView, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+    NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: containerView, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+    NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: containerView, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    scrollView.isScrollEnabled = false
+    loadHTMLContent()
+  }
+  
+  public func setWidgetId(widgetId:String){
+    self.widgetId = widgetId
+  }
+  
+  private func setSiteUrl(siteUrl:String){
+    self.siteUrl = siteUrl
+  }
+  
+  public func setBaseUrl(baseUrl:String){
+    self.baseUrl = baseUrl
+  }
+  
+  public func setWidgetSubId(widgetSubId:[String:String]){
+    self.widgetSubId = widgetSubId
+  }
+  
+  public func setWidgetId(widgetId:String, widgetSubId:[String:String]){
+    self.widgetId = widgetId
+    self.widgetSubId = widgetSubId
+  }
+  
+  public func loadWidget() {
+    if let message = validateWidget(){
+      NSLog(message)
+      return
+    }
+    self.loadHTMLString(generateWidgetHTML(),
+                        baseURL: URL(string: baseUrl ?? defaultBaseURL))
+  }
     
-    private var widgetSubId:[String:String]?
-    override init(frame: CGRect, configuration: WKWebViewConfiguration)
-    {
-        super.init(frame: frame, configuration: configuration)
-        self.navigationDelegate = self
-        self.loadHTMLContent()
+  private func validateWidget() -> String? {
+    if !RCNativeiOSSDK.initiliazed() {
+      return "RCSDK -> SDK not initialzied."
     }
-    
-    //    override func loadView() {
-    //       // if #available(iOS 11, *) {
-    //            let preferences = WKPreferences()
-    //            preferences.javaScriptEnabled = true
-    //            preferences.javaScriptCanOpenWindowsAutomatically = true
-    //            let webConfiguration = WKWebViewConfiguration()
-    //            webConfiguration.preferences = preferences
-    //
-    //            RCNactiveJSWidgetView = WKWebView(frame: .zero, configuration: webConfiguration)
-    //
-    //            let userAgentValue = "Chrome/56.0.0.0 Mobile"
-    //            RCNactiveJSWidgetView.customUserAgent = userAgentValue
-    //            RCNactiveJSWidgetView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    //            RCNactiveJSWidgetView.uiDelegate = self
-    //            view = RCNactiveJSWidgetView
-    //        //}
-    //    }
-    
-    private func loadHTMLContent(){
-        self.htmlWidget =
-        """
-        <!doctype html>
-        <html>
-        <head>
-        <style>
-        html, body { margin:0; padding: 0; }
-        
-        @media (prefers-color-scheme: dark) {
-        html, body {
-        background: #000;
-        }
-        }
-        </style>
-        </head>
-        <body>
-        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><div id=\"rc-widget-27bbf8\" data-rc-widget data-widget-host=\"{widget-host}\" data-endpoint=\"{endpoint}\" data-is-secured=\"{is-secured}\" data-widget-id=\"{widget-id}\" data-sub-ids=\"{sub-ids}\"></div><script src=\"{js-src}\" defer=\"{defer}\"></script>
-        </body>
-        </html>
-        """
+    if self.htmlWidget == nil {
+      return "RCSDK -> RCJSWidgetView: Widget not loaded."
     }
-    //content="initial-scale=1, user-scalable=no,
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+    if self.widgetId == nil  {
+      return "RCSDK -> RCJSWidgetView: WidgetId is required."
     }
-    public func setWidgetId(widgetId:String){
-        self.widgetId = widgetId
+    return nil
+  }
+  
+  private func generateWidgetHTML() -> String {
+    var result = self.htmlWidget!.replacingOccurrences(of: widgetHostKey, with: widgetHostVal)
+    result = result.replacingOccurrences(of: endPointKey, with: endPointVal)
+    result = result.replacingOccurrences(of: isSecuredKey, with: isSecuredVal)
+    result = result.replacingOccurrences(of: widgetIdKey, with: self.widgetId!)
+//        result = result.replacingOccurrences(of: sourceUrlKey, with: self.siteUrl!)
+    result = result.replacingOccurrences(of: jsSrcKey, with: jsSrcVal)
+    result = result.replacingOccurrences(of: deferKey, with: deferVal)
+    if self.widgetSubId != nil {
+      let jsonData = try? JSONSerialization.data(withJSONObject: self.widgetSubId!, options: [])
+      let jsonString = String(data: jsonData!, encoding: .utf8)
+      let replacedJsonString = jsonString!.replacingOccurrences(of: "\"", with: "&quot;")
+      result = result.replacingOccurrences(of: widgetSubIdKey, with: replacedJsonString)
+    } else {
+      result = result.replacingOccurrences(of: widgetSubIdKey, with: "")
     }
-    private func setSiteUrl(siteUrl:String){
-        self.siteUrl = siteUrl
-    }
-    public func setBaseUrl(baseUrl:String){
-        self.baseUrl = baseUrl
-    }
-    public func setWidgetSubId(widgetSubId:[String:String]){
-        self.widgetSubId = widgetSubId
-    }
-    public func setWidgetId(widgetId:String, widgetSubId:[String:String]){
-        self.widgetId = widgetId
-        self.widgetSubId = widgetSubId
-    }
-    public func loadWidget()
-    {
-        let message = validateWidget()
-        if (message == nil){
-            let html = self.generateWidgetHTML();
-            if let baseUrl = self.baseUrl{
-                self.loadHTMLString(html, baseURL: URL.init(string: baseUrl))
-            }else{
-                self.loadHTMLString(html, baseURL: URL.init(string: defaultBaseURL))
-            }
-        }else{
-            NSLog(message!)
-        }
-    }
-    
-    private func validateWidget()->String?{
-        if(!RCNativeiOSSDK.initiliazed()){
-            return "RCSDK -> SDK not initialzied."
-        }
-        if(self.htmlWidget == nil){
-            return "RCSDK -> RCJSWidgetView: Widget not loaded."
-        }
-        if(self.widgetId == nil){
-            return "RCSDK -> RCJSWidgetView: WidgetId is required."
-        }
-        //        if(self.siteUrl == nil){
-        //            return "RCSDK -> RCJSWidgetView: SiteUrl is required."
-        //        }
-        return nil
-    }
-    private func generateWidgetHTML()->String{
-        var result = self.htmlWidget!.replacingOccurrences(of: widgetHostKey, with: widgetHostVal)
-        result = result.replacingOccurrences(of: endPointKey, with: endPointVal)
-        result = result.replacingOccurrences(of: isSecuredKey, with: isSecuredVal)
-        result = result.replacingOccurrences(of: widgetIdKey, with: self.widgetId!)
-        //result = result.replacingOccurrences(of: sourceUrlKey, with: self.siteUrl!)
-        result = result.replacingOccurrences(of: jsSrcKey, with: jsSrcVal)
-        result = result.replacingOccurrences(of: deferKey, with: deferVal)
-        if(self.widgetSubId != nil){
-            let jsonData = try? JSONSerialization.data(withJSONObject: self.widgetSubId!, options: [])
-            let jsonString = String(data: jsonData!, encoding: .utf8)
-            let replacedJsonString = jsonString!.replacingOccurrences(of: "\"", with: "&quot;")
-            result = result.replacingOccurrences(of: widgetSubIdKey, with: replacedJsonString)
-        }else
-        {
-            result = result.replacingOccurrences(of: widgetSubIdKey, with: "")
-        }
-        return result
-    }
+    return result
+  }
 }
 
-extension RCNactiveJSWidgetView: WKNavigationDelegate{
-    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        if navigationAction.navigationType == .linkActivated {
-            guard let url = navigationAction.request.url else {
-                decisionHandler(.allow)
-                return
-            }
-            let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
-            print (url);
-            webView.frame.size.height = 1
-            webView.frame.size = webView.scrollView.contentSize
-            
-            if components?.scheme == "http" || components?.scheme == "https"
-            {
-                UIApplication.shared.open(url)
-                decisionHandler(.cancel)
-            } else {
-                decisionHandler(.allow)
-            }
-        } else if navigationAction.navigationType == .other{
-            print (navigationAction.request.url as Any)
-            decisionHandler(.allow)
-        }else{
-            decisionHandler(.allow)
-        }
+extension RCNactiveJSWidgetView: WKNavigationDelegate {
+  public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+    if navigationAction.navigationType == .linkActivated {
+      guard let url = navigationAction.request.url else { decisionHandler(.allow); return }
+      let components = URLComponents(url: url, resolvingAgainstBaseURL: false)
+      print (url)
+      if components?.scheme == "http" || components?.scheme == "https" {
+        UIApplication.shared.open(url)
+        decisionHandler(.cancel)
+      } else {
+        decisionHandler(.allow)
+      }
+    } else if navigationAction.navigationType == .other {
+      print (navigationAction.request.url as Any)
+      decisionHandler(.allow)
+    } else{
+      decisionHandler(.allow)
     }
-    
-    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        if webView.isLoading == false {
-            webView.evaluateJavaScript("document.body.scrollHeight", completionHandler: { [weak self] (result, error) in
-                if let height = result as? CGFloat {
-                    webView.frame.size.height += height
-                    let height = webView.frame.size.height
-                    print(height)
-                }
-            })
-        }
-    }
+  }
+  
+  public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+    heightHandler.startObservingHeight()
+  }
 }
+
--- RCNativeiOSSDK/RCNativeiOSSDK.swift
+++ RCNativeiOSSDK/RCNativeiOSSDK.swift
@@ -9,28 +9,22 @@
 import UIKit
 
 public final class RCNativeiOSSDK: NSObject {
-    private static var instance: RCNativeiOSSDK!
-    private var initiliazed = false
-
-    private override init() {
+  private static var instance: RCNativeiOSSDK!
+  private var initiliazed = false
+  
+  private override init() {}
+  
+  public class func setup(){
+    if instance == nil {
+      instance = RCNativeiOSSDK()
     }
-    public class func setup(){
-        if (instance == nil){
-            instance = RCNativeiOSSDK()
-        }
-    }
-    private class func validateSDK()->Bool{
-        if(instance != nil){
-            return true
-        }else{
-            return false
-        }
-    }
-    public class func initiliazed()->Bool{
-        if(validateSDK()){
-            return true
-        }else{
-            return false
-        }
-    }
+  }
+  
+  private class func validateSDK() -> Bool {
+    return instance != nil
+  }
+  
+  public class func initiliazed() -> Bool {
+    return validateSDK()
+  }
 }
